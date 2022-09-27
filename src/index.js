61ad6a3bb2298a50ec1647166beeb61db39a3665
DMSofwareDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-09-27 08:58:16 +0700

    13*13

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+const sizeOfBoard = 13;
 function Square(props) {
   const className = "square" + (props.highlight ? " highlight" : "");
 
@@ -14,7 +15,6 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     const winLine = this.props.winLine;
-    console.log("props", this.props);
     return (
       <Square
         value={this.props.squares[i]}
@@ -25,7 +25,6 @@ class Board extends React.Component {
     );
   }
   createABoard() {
-    const sizeOfBoard = 3;
     let squares = [];
     let row = [];
     for (let i = 0; i < sizeOfBoard; i++) {
@@ -52,7 +51,7 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null),
+          squares: Array(sizeOfBoard * sizeOfBoard).fill(null),
           latestMoveSquare: 0,
         },
       ],
@@ -158,33 +157,182 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return {
-        winner: squares[a],
-        line: lines[i],
-        isDrawing: false,
-      };
+function handleWin(row, col, type, winner) {
+  let arr = [];
+  if (type === "row") {
+    arr.push(
+      row * sizeOfBoard + col,
+      row * sizeOfBoard + col + 1,
+      row * sizeOfBoard + col + 2,
+      row * sizeOfBoard + col + 3,
+      row * sizeOfBoard + col + 4
+    );
+    return {
+      winner: winner,
+      line: arr,
+      isDrawing: false,
+    };
+  } else if (type === "col") {
+    arr.push(
+      col + row * sizeOfBoard,
+      col + (row + 1) * sizeOfBoard,
+      col + (row + 2) * sizeOfBoard,
+      col + (row + 3) * sizeOfBoard,
+      col + (row + 4) * sizeOfBoard
+    );
+    return {
+      winner: winner,
+      line: arr,
+      isDrawing: false,
+    };
+  } else if (type === "main-diagonal") {
+    arr.push(
+      col * sizeOfBoard + row,
+      (col + 1) * sizeOfBoard + row + 1,
+      (col + 2) * sizeOfBoard + row + 2,
+      (col + 3) * sizeOfBoard + row + 3,
+      (col + 4) * sizeOfBoard + row + 4
+    );
+    return {
+      winner: winner,
+      line: arr,
+      isDrawing: false,
+    };
+  } else if (type === "sub-diagonal") {
+    arr.push(
+      row * sizeOfBoard + col,
+      (row + 1) * sizeOfBoard + col - 1,
+      (row + 2) * sizeOfBoard + col - 2,
+      (row + 3) * sizeOfBoard + col - 3,
+      (row + 4) * sizeOfBoard + col - 4
+    );
+    return {
+      winner: winner,
+      line: arr,
+      isDrawing: false,
+    };
+  }
+  return {
+    winner: null,
+    line: null,
+    isDrawing: false,
+  };
+}
+function calculateWinner(board) {
+  // hang ngang
+  for (let row = 0; row < sizeOfBoard; row++) {
+    for (let col = 0; col < sizeOfBoard - 4; col++) {
+      if (
+        "XXXXX" ===
+        "" +
+          board[row * sizeOfBoard + col] +
+          board[row * sizeOfBoard + col + 1] +
+          board[row * sizeOfBoard + col + 2] +
+          board[row * sizeOfBoard + col + 3] +
+          board[row * sizeOfBoard + col + 4]
+      ) {
+        return handleWin(row, col, "row", "X");
+      } else if (
+        "OOOOO" ===
+        "" +
+          board[row * sizeOfBoard + col] +
+          board[row * sizeOfBoard + col + 1] +
+          board[row * sizeOfBoard + col + 2] +
+          board[row * sizeOfBoard + col + 3] +
+          board[row * sizeOfBoard + col + 4]
+      ) {
+        return handleWin(row, col, "row", "O");
+      }
+    }
+  }
+  // hang doc
+  for (let col = 0; col < sizeOfBoard; col++) {
+    for (let row = 0; row < sizeOfBoard - 4; row++) {
+      if (
+        "XXXXX" ===
+        "" +
+          board[col + row * sizeOfBoard] +
+          board[col + (row + 1) * sizeOfBoard] +
+          board[col + (row + 2) * sizeOfBoard] +
+          board[col + (row + 3) * sizeOfBoard] +
+          board[col + (row + 4) * sizeOfBoard]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "col", "X");
+      } else if (
+        "OOOOO" ===
+        "" +
+          board[col + row * sizeOfBoard] +
+          board[col + (row + 1) * sizeOfBoard] +
+          board[col + (row + 2) * sizeOfBoard] +
+          board[col + (row + 3) * sizeOfBoard] +
+          board[col + (row + 4) * sizeOfBoard]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "col", "O");
+      }
+    }
+  }
+  for (let row = 0; row < sizeOfBoard - 4; row++) {
+    for (let col = 0; col < sizeOfBoard - 4; col++) {
+      if (
+        "XXXXX" ===
+        "" +
+          board[col * sizeOfBoard + row] +
+          board[(col + 1) * sizeOfBoard + row + 1] +
+          board[(col + 2) * sizeOfBoard + row + 2] +
+          board[(col + 3) * sizeOfBoard + row + 3] +
+          board[(col + 4) * sizeOfBoard + row + 4]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "main-diagonal", "X");
+      } else if (
+        "OOOOO" ===
+        "" +
+          board[col * sizeOfBoard + row] +
+          board[(col + 1) * sizeOfBoard + row + 1] +
+          board[(col + 2) * sizeOfBoard + row + 2] +
+          board[(col + 3) * sizeOfBoard + row + 3] +
+          board[(col + 4) * sizeOfBoard + row + 4]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "main-diagonal", "O");
+      }
+    }
+  }
+  for (let row = 0; row < sizeOfBoard - 4; row++) {
+    for (let col = 0; col < sizeOfBoard; col++) {
+      if (
+        "XXXXX" ===
+        "" +
+          board[row * sizeOfBoard + col] +
+          board[(row + 1) * sizeOfBoard + col - 1] +
+          board[(row + 2) * sizeOfBoard + col - 2] +
+          board[(row + 3) * sizeOfBoard + col - 3] +
+          board[(row + 4) * sizeOfBoard + col - 4]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "sub-diagonal", "X");
+      } else if (
+        "OOOOO" ===
+        "" +
+          board[row * sizeOfBoard + col] +
+          board[(row + 1) * sizeOfBoard + col - 1] +
+          board[(row + 2) * sizeOfBoard + col - 2] +
+          board[(row + 3) * sizeOfBoard + col - 3] +
+          board[(row + 4) * sizeOfBoard + col - 4]
+      ) {
+        console.log("compare success");
+        return handleWin(row, col, "sub-diagonal", "O");
+      }
     }
   }
   let isDrawing = true;
-  for (let i = 0; i < squares.length; i++) {
-    if (squares[i] === null) {
-      isDrawing = false;
-      break;
+  for (let i = 0; i < sizeOfBoard; i++) {
+    for (let j = 0; j < sizeOfBoard; j++) {
+      if (board[i] === "") {
+        isDrawing = false;
+      }
     }
   }
   return {
